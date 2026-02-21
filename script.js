04ca41b40198da11fbcc98f08d0726dbac8e384d
libananking-gif <libananking@gmail.com>
2026-02-21 19:51:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function submitOrder() {
     body += "TOTAL AMOUNT: PHP " + totalAmount.toFixed(2);
 
     // Palitan mo ito ng email ni Arjay
-    let arjayEmail = "arjay@example.com"; 
+    let arjayEmail = "libananking@gmail.com"; 
     let mailtoLink = "mailto:" + arjayEmail + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
 
     window.location.href = mailtoLink;
